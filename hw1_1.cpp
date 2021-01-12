852a9b6e5cf5ecdae1b6f066d2bf83e54340b747
Wanprasert <77339479+Wanprasert@users.noreply.github.com>
2021-01-12 22:07:10 +0700

    Add files via upload

---
+++ hw1_1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << endl;
+	cout << " 1st Term of 2020	/6206021621171	Wanprasert Plongsesthee" << endl;
+	cout << endl; cout << endl;  
+	cout << "---------------------------------------------------------------------------" << endl;
+	cout << "| Subject ID | Subject name			| Score (100)| Grade |" << endl;
+	cout << "---------------------------------------------------------------------------" << endl;
+	cout << "| 060243107  | COMPUTER ARCHITECTURE		|     53     |   D   |" << endl;
+	cout << "| 060243108  | DATABASE SYSTEM     		|     66     |   C+  |" << endl;
+	cout << "| 060243109  | COMPUTER NETWORK AND COMMUNI	|     70     |   B   |" << endl;
+	cout << "| 080303608  | CITIZENSHIP AND SOCIAL RESPO	|     75     |   B+  |" << endl;
+	cout << "| 080303609  | HEALTHY LIFE			|     77     |   B+  |" << endl;
+	cout << "---------------------------------------------------------------------------" << endl;
+	cout << "|   TOTAL    | Subject = 5     			| Average = 2.7  |" << endl;
+	cout << "==============================================" << endl;
+	cout << endl; cout << endl; cout << endl;
+	cout << "KMUTNB" << endl; cout << endl;
+
+	return(0);
+}     
